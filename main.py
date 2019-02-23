62dc32d1fb882ab6186f7a1717d500bfac17c809
Luis Guitron <legl_1995@hotmail.com>
2019-02-23 14:45:23 -0600

    Change result printing

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from simulate_league import simulate_league
 from update_ratings import update_all_ratings
 
 update_all_ratings()
-results        = simulate_league(simulation_amount = 5000)
+results        = simulate_league(simulation_amount = 50000)
 sorted_results = sorted(results.items(), key=operator.itemgetter(1), reverse=True)
-print(sorted_results)
+
+for row in sorted_results:
+    print(row)
